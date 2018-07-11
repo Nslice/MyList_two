8c39c5ab60960b57996cb541f7f4a189b5cc3376
Talgat <tairovtalgat@gmail.com>
2018-07-11 23:47:46 +0600

    initial commit

---
+++ MyList.h
@@ -0,0 +1,280 @@
+﻿#ifndef MY_LIST_H_INCLUDED_
+#define MY_LIST_H_INCLUDED_
+
+template<typename Type>
+class List
+{
+private:
+	struct Node
+	{
+		Type data;
+		Node* pNext;
+		Node* pPrev;
+
+		Node(Type data = Type(), Node* pNext = nullptr, Node* pPrev = nullptr)
+		{
+			this->data = data;
+			this->pNext = pNext;
+			this->pPrev = pPrev;
+		}
+	};
+
+	Node* head;
+	Node* tail;
+	int size;
+
+	List(const List&) = delete;      //не точно
+	List& operator=(const List&) = delete;
+public:
+	List();
+	~List();
+
+	int get_size() const { return size; }
+	bool empty() const { return !size; }
+
+	Type& front();
+	const Type& front() const;
+	Type& back();
+	const Type& back() const;
+
+	void push_back(const Type& data);
+	void push_front(const Type& data);
+	void pop_back();
+	void pop_front();
+	void clear();
+	void insertAt(const Type& data, int index);
+	void removeAt(int index);
+	Type& operator[](int index);
+	const Type& operator[](int index) const;
+};
+
+template<typename Type>
+List<Type>::List() : head(nullptr), tail(nullptr), size(0) {}
+
+template<typename Type>
+List<Type>::~List()
+{
+	clear();
+}
+
+template<typename Type>
+Type& List<Type>::front()
+{
+	if (!size)
+		throw std::exception("list iterator not deferencable");
+	return head->data;
+}
+template<typename Type>
+const Type& List<Type>::front() const
+{
+	if (!size)
+		throw std::exception("list iterator not deferencable");
+	return head->data;
+}
+
+template<typename Type>
+Type& List<Type>::back()
+{
+	if (!size)
+		throw std::exception("list iterator not deferencable");
+	return tail->data;
+}
+template<typename Type>
+const Type& List<Type>::back() const
+{
+	if (!size)
+		throw std::exception("list iterator not deferencable");
+	return tail->data;
+}
+
+template<typename Type>
+void List<Type>::push_back(const Type& data)
+{
+	if (head == nullptr)
+	{
+		head = new Node(data);
+		tail = head;
+	}
+	else
+	{
+		tail->pNext = new Node(data, nullptr, tail);
+		tail = tail->pNext;
+	}
+	size++;
+}
+
+template<typename Type>
+void List<Type>::push_front(const Type& data)
+{
+	if (head == nullptr)
+	{
+		head = new Node(data);
+		tail = head;
+	}
+	else
+	{
+		head->pPrev = new Node(data, head);
+		head = head->pPrev;
+	}
+	size++;
+}
+
+template<typename Type>
+void List<Type>::pop_back()
+{
+	if (head == nullptr)
+		return;
+
+	if (head == tail)
+	{
+		delete head;
+		head = tail = nullptr;
+	}
+	else
+	{
+		tail = tail->pPrev;
+		delete tail->pNext;
+		tail->pNext = nullptr;
+	}
+	size--;
+}
+
+template<typename Type>
+void List<Type>::pop_front()
+{
+	if (head == nullptr)
+		return;
+
+	if (head == tail)
+	{
+		delete head;
+		head = tail = nullptr;
+	}
+	else
+	{
+		head = head->pNext;
+		delete head->pPrev;
+		head->pPrev = nullptr;
+	}
+	size--;
+}
+
+template<typename Type>
+void List<Type>::clear()
+{
+	while (size)
+		pop_front();
+}
+
+template<typename Type>
+void List<Type>::insertAt(const Type& data, int index)
+{
+	if (index < 0 || index > size) throw std::out_of_range("Invalid index");
+
+	if (!index)
+		push_front(data);
+	else if (index == size)
+		push_back(data);
+	else
+	{
+		Node* need;
+		if (index >= size / 2)
+		{
+			need = tail;
+			for (int i = 0; i < (size - index); i++)
+				need = need->pPrev;
+			need->pNext = need->pNext->pPrev = new Node(data, need->pNext, need);
+		}
+		else
+		{
+			need = head;
+			for (int i = 0; i < index - 1; i++)
+				need = need->pNext;
+			need->pNext = need->pNext->pPrev = new Node(data, need->pNext, need);
+		}
+		size++;
+	}	
+}
+
+template<typename Type>
+void List<Type>::removeAt(int index)
+{
+	if (index < 0 || index >= size) throw std::out_of_range("Invalid index");
+
+	if (!index)
+		pop_front();
+	else if (index == size - 1)
+		pop_back();
+	else
+	{
+		Node* need;
+		if (index >= size / 2)
+		{
+			need = tail;
+			for (int i = 0; i < (size - index); i++)
+				need = need->pPrev;
+			Node* toDelete = need->pNext;
+			need->pNext = toDelete->pNext;
+			need->pNext->pPrev = need;
+			delete toDelete;
+		}
+		else
+		{
+			need = head;
+			for (int i = 0; i < (index - 1); i++)
+				need = need->pNext;
+			Node* toDelete = need->pNext;
+			need->pNext = toDelete->pNext;
+			need->pNext->pPrev = need;
+			delete toDelete;
+		}
+		size--;
+	}
+}
+
+template<typename Type>
+Type& List<Type>::operator[](int index)
+{
+	if (index < 0 || index >= size) throw std::out_of_range("Invalid index");
+
+	Node* need;
+	if (index >= size / 2)
+	{
+		need = tail;
+		for (int i = 0; i < (size - index - 1); i++)
+			need = need->pPrev;
+	}
+	else
+	{
+		need = head;
+		for (int i = 0; i < index; i++)
+			need = need->pNext;
+	}
+
+	return need->data;
+}
+
+template<typename Type>
+const Type& List<Type>::operator[](int index) const
+{
+	if (index < 0 || index >= size) throw std::out_of_range("Invalid index");
+
+	Node* need;
+	if (index >= size / 2)
+	{
+		need = tail;
+		for (int i = 0; i < (size - index - 1); i++)
+			need = need->pPrev;
+	}
+	else
+	{
+		need = head;
+		for (int i = 0; i < index; i++)
+			need = need->pNext;
+	}
+
+	return need->data;
+}
+
+
+#endif // !MY_LIST_H_INCLUDED_
---
+++ main.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "MyList.h"
+
+using namespace std;
+
+struct Student
+{
+	string name;
+	string group;
+	int year;
+
+	friend ostream& operator<<(ostream& os, const Student& a)
+	{
+		os << setw(15) << a.name << setw(15) << a.group <<
+			setw(8) << a.year;
+		return os;
+	}
+};
+
+template<class T>
+void show(const List<T>& ml);
+
+void fill(List<Student>& ml, string path);
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	List<Student> list;
+
+	fill(list, "students.txt");
+
+	show(list);
+
+	try
+	{
+		cout << "------------------------\n";
+		Student a{ "Boy", "M", 2005 };
+		list.removeAt(7);
+		list.insertAt(a, 7);
+		show(list);
+
+
+
+		list.pop_back();
+		list.pop_front();
+		cout << list.front() << endl;
+		cout << list.back() << endl;
+		
+		cout << "size: " << list.get_size() << endl;
+	}
+	catch (const std::exception& ex) { cout << ex.what() << endl; }
+
+
+
+	return 0;
+}
+
+template<class T>
+void show(const List<T>& ml)
+{
+	for (int i = 0; i < ml.get_size(); i++)
+		cout << setw(5) << i + 1 << ml[i] << endl;
+}
+
+void fill(List<Student>& ml, string path)
+{
+	Student temp;
+	ifstream fcin;
+	fcin.open(path);
+	while (!fcin.eof())
+	{
+		fcin >> temp.name >> temp.group >> temp.year;
+		ml.push_back(temp);
+	}
+}
+
+/***
+Node
+List()
+~List()
+get_size
+empty
+front / front const
+back  / back const
+push_back
+push_front
+pop_back
+pop_front
+clear
+insert
+removeAt
+operator[] / operator[] const
+
+
+
+
+
+
+*/
